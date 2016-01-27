668b8e6a0fd6dfd512a9d8944df85d8431ed20c1
Jack Higgins <jackhiggins07@gmail.com>
2016-01-27 18:02:28 +0000

    Added Rating type

--- src/Parser/RatingsMatrixParser.hs
+++ src/Parser/RatingsMatrixParser.hs
@@ -14,14 +14,19 @@ import Text.Parsec.String
 
 import Text.ParserCombinators.Parsec.Number
 
+rankings :: IO (Either ParseError [Rank])
+ratings  :: IO (Either ParseError [Rating])
+
 rankings = parseFromFile (many ranking) "data/rating-ranks"
 ratings  = parseFromFile (many rating)  "data/sparse-rating-matrix"
 
 ranking :: Parsec String st Rank
-ranking = Rank <$> identity <* spaces <*> nat <* spaces
+ranking = Rank
+  <$> identity <* spaces
+  <*> nat      <* spaces
 
-rating :: Floating a => Parsec String st (String, String, a)
-rating = (\a b c -> (a,b,c))
+rating :: Floating a => Parsec String st Rating
+rating = Rating
   <$> identity       <* spaces
   <*> identity       <* spaces
   <*> floating2 True <* spaces
--- src/Types/Marker.hs
+++ src/Types/Marker.hs
@@ -3,3 +3,8 @@ module Types.Marker where
 data Rank = Rank { id :: String
                  , rank :: Int
                  } deriving (Eq, Show)
+
+data Rating = Rating { rater :: String
+                     , ratee :: String
+                     , score :: Double
+                     } deriving (Eq, Show)
