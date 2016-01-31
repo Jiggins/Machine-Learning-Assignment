db78107a82c09833a728a1efae138e924a847555
Jack Higgins <jackhiggins07@gmail.com>
2016-01-31 13:59:19 +0000

    Building the Matrix

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Parser.RatingsMatrixParser
 import Types.Marker
 import Types.MatrixEntry
 
+import Nmeric.LinearAlgebra
 import System.Exit
 
 -- | Runs the parser, the program exits if a
@@ -13,7 +14,14 @@ runParse parser = parser >>= \p -> case p of
   Right a -> return a
   Left a -> die (show a)
 
+getMap = do
+  rankings <- runParse rankings
+  ratings  <- runParse ratings
+  return $ buildMap rankings ratings
+
 main = do
   rankings <- runParse rankings
-  ratings <- runParse ratings
-  print $ buildMatrix rankings ratings
+  ratings  <- runParse ratings
+  map      <- buildMap rankings ratings
+  matrix   <- buildMatrix ratings map
+  print $ matrix
--- src/Matrix/MatrixBuilder.hs
+++ src/Matrix/MatrixBuilder.hs
@@ -1,12 +1,20 @@
-module Matrix.MatrixBuilder (buildMatrix) where 
+module Matrix.MatrixBuilder (buildMap, buildMatrix) where
 
 import Types.Marker
 import Types.MatrixEntry
 
-import Data.Map.Strict (Map (..))
+import Data.Map.Strict (Map (..), (!))
 import qualified Data.Map.Strict as Map
 
-buildRankMap :: [Rank] -> Map.Map String Int
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Numeric.LinearAlgebra hiding ((!))
+
+buildMap :: [Rank] -> [Rating] -> Map String MatrixEntry
+buildMap ranks = mergeMaps (buildRankMap ranks) . buildRatingsMap
+
+buildRankMap :: [Rank] -> Map String Int
 buildRankMap = foldr (\(Rank id rank) -> Map.insert id rank) Map.empty
 
 buildRatingsMap :: [Rating] -> Map String [(String, Double)]
@@ -16,5 +24,23 @@ buildRatingsMap = foldr insertRatings Map.empty
 mergeMaps :: Map String Int -> Map String [(String, Double)] -> Map String MatrixEntry
 mergeMaps ranks = Map.mapWithKey (\k -> MatrixEntry k (Map.lookup k ranks))
 
-buildMatrix :: [Rank] -> [Rating] -> Map String MatrixEntry
-buildMatrix ranks = mergeMaps (buildRankMap ranks) . buildRatingsMap
+rateeKeyMap, raterKeyMap :: [Rating] -> Map String Int
+rateeKeyMap = buildKeyMap rateeId
+raterKeyMap = buildKeyMap raterId
+
+buildKeyMap :: (Enum v, Num v, Ord k) => (a -> k) -> [a] -> Map k v
+buildKeyMap f = Map.fromList . flip zip [0..] . Set.toList . keySet f
+  where keySet f = foldr Set.insert Set.empty . map f
+
+buildMatrix :: [Rating] -> Map String MatrixEntry -> Matrix Double
+buildMatrix rs = assoc (height,width) 0 . assocList rs
+  where height = 36
+        width  = 37
+
+assocList :: [Rating] -> Map String MatrixEntry -> [((Int, Int), Double)]
+assocList ratings = concatMap (map swap . f . (\(a,b) -> (raterKeys ! a, scores b))) . Map.toList
+  where rateeKeys = rateeKeyMap ratings
+        raterKeys = raterKeyMap ratings
+        swap (a,(b,c)) = ((a, rateeKeys ! b), c)
+        f (a,b) = map (\x -> (a,x)) b
+        fstMap f (a,b) = (f a, b)
